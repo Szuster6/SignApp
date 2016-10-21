9a39ea8c4f8c79bdccff3b3fe6dc89ed46b0dd94
Bodzaro <bojarlukasz@gmail.com>
2016-10-21 19:18:41 +0200

    Męcze się już jakieś 8h z tym gównem i mi nie wychodzi, sprawdźcie sami ! UWAGA aplikacja całkiem zmieniona ! - Łukasz Bojarski

--- app/src/main/java/com/example/mklos/signapp/MainActivity.java
+++ app/src/main/java/com/example/mklos/signapp/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.mklos.signapp;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.hardware.Camera;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,20 +19,68 @@ import android.view.MenuItem;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import org.w3c.dom.Text;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class MainActivity extends Activity implements SurfaceHolder.Callback {
+public class MainActivity extends Activity implements CvCameraViewListener2 {
 
-    private static final String TAG = "MainActivity";
+    //private static final String TAG = "MainActivity";
 
-    static {
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private SeekBar seekbar;
+    private TextView text;
+
+    private static final int VIEW_MODE_RGBA = 0;
+    private static final int VIEW_MODE_CANNY = 1;
+    private static final int VIEW_MODE_THRESH = 2;
+
+    private int mViewMode;
+
+    private Mat mRgba;
+    private Mat mIntermediateMat;
+    private Mat mGray;
+
+    private MenuItem mItemPreviewRGBA;
+    private MenuItem mItemPreviewCanny;
+    private MenuItem mItemPreviewThresh;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this){
+        @Override
+        public void onManagerConnected(int status){
+            switch (status){
+                case LoaderCallbackInterface.SUCCESS: {
+                    Log.i("Android Tutorial", "OpenCV loaded successfully");
+                    System.loadLibrary("mixed_sample");
+                    mOpenCvCameraView.enableView();
+                } break;
+                default: {
+                    super.onManagerConnected(status);
+                }break;
+            }
+        }
+    };
+
+    /*static {
         if (!OpenCVLoader.initDebug()) {
             Log.d(TAG, "OpenCV not loaded");
         } else {
@@ -37,25 +91,158 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
     Camera camera;
     SurfaceView surfaceView;
     SurfaceHolder surfaceHolder;
-
+    TextView textView;
     Camera.PictureCallback rawCallback;
     Camera.ShutterCallback shutterCallback;
-    Camera.PictureCallback jpegCallback;
-
+    Camera.PictureCallback jpegCallback;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //getPermissionToReadCamera();
+        setContentView(R.layout.surface_view);
 
-        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
-        surfaceHolder = surfaceView.getHolder();
+        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        surfaceHolder.addCallback(this);
-        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        //surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        //textView = (TextView) findViewById(R.id.textView);
+
+        //surfaceHolder = surfaceView.getHolder();
+
+        //surfaceHolder.addCallback(this);
+        //surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.tutorial_main_view);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+
+        seekbar = (SeekBar)findViewById(R.id.seekBar);
+        seekbar.setMax(255);
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                text.setText(Integer.toString(progress));
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar arg0) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar arg0) {
+            }
+
+        });
+        text = (TextView)findViewById(R.id.sliderValue);
     }
 
-    public void refreshCamera() {
+    @Override
+    public void onSaveInstanceState(Bundle saveInstanceState){
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        mItemPreviewRGBA = menu.add("Preview RGBA");
+        mItemPreviewCanny = menu.add("Canny Edges");
+        mItemPreviewThresh = menu.add("Threshold");
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        if (item == mItemPreviewRGBA){
+            mViewMode = VIEW_MODE_RGBA;
+        } else if (item == mItemPreviewCanny){
+            mViewMode = VIEW_MODE_CANNY;
+        } else if (item == mItemPreviewThresh){
+            mViewMode = VIEW_MODE_THRESH;
+        }
+        return true;
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        mIntermediateMat = new Mat(height, width, CvType.CV_8UC4);
+        mGray = new Mat(height, width, CvType.CV_8UC1);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+        mIntermediateMat.release();
+        mGray.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+        final int viewMode = mViewMode;
+        switch (viewMode) {
+            case VIEW_MODE_RGBA:
+                mRgba = inputFrame.rgba();
+                break;
+
+            case VIEW_MODE_CANNY:
+                mRgba = inputFrame.rgba();
+                Imgproc.Canny(inputFrame.gray(), mIntermediateMat, 80, 100);
+                Imgproc.cvtColor(mIntermediateMat, mRgba, Imgproc.COLOR_GRAY2RGBA, 4);
+                break;
+
+            case VIEW_MODE_THRESH:
+                Imgproc.threshold(inputFrame.gray(), mRgba, seekbar.getProgress(),
+                        seekbar.getMax(), Imgproc.THRESH_BINARY);
+                break;
+        }
+
+        return mRgba;
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        if (mOpenCvCameraView != null){
+            mOpenCvCameraView.disableView();
+        }
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_3, this, mLoaderCallback);
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        if (mOpenCvCameraView != null){
+            mOpenCvCameraView.disableView();
+        }
+    }
+
+/*
+    @TargetApi(23)
+    public void getPermissionToReadCamera() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (shouldShowRequestPermissionRationale(
+                    Manifest.permission.CAMERA)) {
+            }
+            requestPermissions(new String[]{Manifest.permission.CAMERA},
+                    CAMERA_PERMISSIONS_REQUEST);}}
+    @Override
+    @TargetApi(23)
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        if (requestCode == CAMERA_PERMISSIONS_REQUEST) {
+            if (grantResults.length == 1 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Camera permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show();
+                    }}}
+
+*/
+    /*public void refreshCamera() {
         if (surfaceHolder.getSurface() == null) {
             return;
         }
@@ -71,11 +258,6 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
             camera.startPreview();
         }
         catch (Exception e) {}
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
     }
 
     @Override
@@ -116,5 +298,5 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
         camera.stopPreview();
         camera.release();
         camera = null;
-    }
+    }*/
 }
